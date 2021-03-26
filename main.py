89e007b3aabe807a04da1799001d1d85be9a89cd
Joey Cadieux <instructor.joeycadieux@gmail.com>
2021-03-26 23:51:54 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,54 @@
+import os
+from tkinter import filedialog, colorchooser, font
+from tkinter.messagebox import *
+from tkinter.filedialog import *
+from tkinter import *
+
+
+def change_color():
+    pass
+
+def change_font(*args)
+  pass
+
+def new_file():
+    pass
+
+
+def open_file():
+    pass
+
+def save_file():
+    pass
+
+
+def cut():
+    pass
+
+
+def copy():
+    pass
+
+
+def paste():
+    pass
+
+def about():
+    pass
+
+
+def quit():
+    pass
+
+
+window = Tk()
+window.title(Text Editor Program)
+file = None
+
+
+window_width = 500
+window_height = 500
+
+
+
+window.mainloop()
